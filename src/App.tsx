4d27ef4a42e0b9c83d2942b14132cab6dd8ed5c3
Bamarcheti <bmarchetifiorin@gmail.com>
2022-10-21 09:33:40 -0400

    fix: correção eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
-/* eslint-disable linebreak-style */
 import { BrowserRouter } from 'react-router-dom';
-
 import { AppRoutes } from './routes';
 import { AppThemeProvider } from './shared/contexts';
 import MainLayout from './shared/layouts/MainLayout';
 
-
 export const App = () => {
   return (
-    // eslint-disable-next-line react/jsx-filename-extension
     <AppThemeProvider>
       <BrowserRouter>
         <MainLayout>
