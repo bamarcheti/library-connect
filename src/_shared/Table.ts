0ddaf4d7a9cb541faf3b0de15731549db880f2ec
Bárbara Marcheti Fiorin <bfiorin@psgtecnologiaaplicada.com.br>
2024-01-11 09:05:54 -0400

    feat: Criando interfaces da Tabela

---
+++ src/_shared/Table.ts
@@ -0,0 +1,23 @@
+import { Author } from '../modules/author/services/authorService';
+
+export interface TableColumn {
+  label: string;
+  dataKey: string;
+  align?: 'left' | 'center' | 'right';
+}
+
+interface TableData {
+  id: string;
+  title: string;
+  qtdPages: number;
+  author: Author;
+  isDigital: boolean;
+  sizeInKBytes: number;
+  kindleCompatible: boolean;
+  publishDate: string;
+}
+
+interface TableProps<T> {
+  data: T[];
+  columns: TableColumn[];
+}
