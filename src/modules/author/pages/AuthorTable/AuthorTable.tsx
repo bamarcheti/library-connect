fd68780eb50ecc55789edfa8c2b932099da549b7
Bárbara Marcheti Fiorin <bfiorin@psgtecnologiaaplicada.com.br>
2024-01-11 09:05:06 -0400

    fix: Alterando componentes Table e deixando menos genérico

--- src/modules/author/pages/AuthorTable/AuthorTable.tsx
+++ src/modules/author/pages/AuthorTable/AuthorTable.tsx
@@ -1,19 +1,60 @@
-import ContentTable, {
-  TableColumn,
-} from '../../../../shared/components/ContentTable';
+import {
+  Card,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from '@mui/material';
+import { TableColumn } from '../../../../_shared/Table';
 
 type Author = {
   name: string;
   qtdBooks: number;
 };
 
+const authorsList: Author[] = [
+  { name: 'Luis', qtdBooks: 8 },
+  { name: 'Bárbara', qtdBooks: 3 },
+  { name: 'Marcelo', qtdBooks: 1 },
+  { name: 'Teodoro Marques', qtdBooks: 1 },
+];
+
 const authorColumns: TableColumn[] = [
   { label: 'Nome', dataKey: 'name' },
   { label: 'Quantidade de livros', dataKey: 'qtdBooks', align: 'center' },
 ];
 
-const AuthorsTable: React.FC<{ authorsList: Author[] }> = ({ authorsList }) => {
-  return <ContentTable data={authorsList} columns={authorColumns} />;
+const AuthorsTable = () => {
+  return (
+    <Card>
+      <TableContainer>
+        <Table sx={{ minWidth: 488 }} size="small" aria-sort="none">
+          <TableHead>
+            <TableRow>
+              {authorColumns.map((column, index) => (
+                <TableCell key={index} align={column.align || 'left'}>
+                  {column.label}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {authorsList.map((author, index) => (
+              <TableRow
+                key={index}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <TableCell component="th">{author.name}</TableCell>
+                <TableCell align="center">{author.qtdBooks}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Card>
+  );
 };
 
 export default AuthorsTable;
--- src/modules/book/pages/BookTable/BookTable.tsx
+++ src/modules/book/pages/BookTable/BookTable.tsx
@@ -1,6 +1,14 @@
-import ContentTable, {
-  TableColumn,
-} from '../../../../shared/components/ContentTable';
+import {
+  Card,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from '@mui/material';
+import dayjs from 'dayjs';
+import { TableColumn } from '../../../../_shared/Table';
 
 type Book = {
   title: string;
@@ -9,6 +17,33 @@ type Book = {
   publishDate: string;
 };
 
+const booksList: Book[] = [
+  {
+    title: 'Clean Code',
+    qtdPages: 500,
+    author: { name: 'Luis' },
+    publishDate: '12/09/2021',
+  },
+  {
+    title: 'Arquitetura Limpa',
+    qtdPages: 500,
+    author: { name: 'Guilherme' },
+    publishDate: '01/23/2021',
+  },
+  {
+    title: 'Harry Architecture',
+    qtdPages: 4000,
+    author: { name: 'Luis' },
+    publishDate: '03/08/1976',
+  },
+  {
+    title: 'Aonde caminhava o homem',
+    qtdPages: 16,
+    author: { name: 'Teodoro Marques' },
+    publishDate: '05/11/2020',
+  },
+];
+
 const bookColumns: TableColumn[] = [
   { label: 'Título', dataKey: 'title' },
   { label: 'Qtd páginas', dataKey: 'qtdPages', align: 'center' },
@@ -16,8 +51,39 @@ const bookColumns: TableColumn[] = [
   { label: 'Data da Publicação', dataKey: 'publishDate', align: 'center' },
 ];
 
-const BookTable: React.FC<{ booksList: Book[] }> = ({ booksList }) => {
-  return <ContentTable data={booksList} columns={bookColumns} />;
+const BookTable = () => {
+  return (
+    <Card>
+      <TableContainer>
+        <Table sx={{ minWidth: 600 }} size="small" aria-sort="none">
+          <TableHead>
+            <TableRow>
+              {bookColumns.map((column, index) => (
+                <TableCell key={index} align={column.align || 'left'}>
+                  {column.label}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {booksList.map((book, index) => (
+              <TableRow
+                key={index}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <TableCell component="th">{book.title}</TableCell>
+                <TableCell align="center">{book.qtdPages}</TableCell>
+                <TableCell align="center">{book.author.name}</TableCell>
+                <TableCell align="center">
+                  {dayjs(book.publishDate).format('DD/MM/YYYY')}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Card>
+  );
 };
 
 export default BookTable;
